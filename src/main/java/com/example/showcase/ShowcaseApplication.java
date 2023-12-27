2a4f76076724c316a21589b8ff4ef8c581acabac
Xingliang99 <antonyliu99@gmail.com>
2023-12-27 13:56:10 +0800

    add index

--- src/main/java/com/example/showcase/ShowcaseApplication.java
+++ src/main/java/com/example/showcase/ShowcaseApplication.java
@@ -2,12 +2,20 @@ package com.example.showcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ShowcaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShowcaseApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String index(){
+		return "Hello";
+	}
 }
