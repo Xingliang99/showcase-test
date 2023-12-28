267d1032e2c40cd5d7de18a21fe1013577443d22
Xingliang99 <antonyliu99@gmail.com>
2023-12-28 22:21:53 +0800

    add cors

--- src/main/java/com/example/showcase/ShowcaseApplication.java
+++ src/main/java/com/example/showcase/ShowcaseApplication.java
@@ -2,12 +2,14 @@ package com.example.showcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin
 public class ShowcaseApplication {
 
 	public static void main(String[] args) {
